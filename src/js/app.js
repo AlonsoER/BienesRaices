5d64aa90d2721efcdae0d81cd43e47a6d9a10183
AlonsoER <alonsoer2343@gmail.com>
2022-05-26 17:10:29 -0600

    Añadiendo Dark-Mode

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,7 @@
 document.addEventListener('DOMContentLoaded', function(){
 
     eventListeners();
+    darkMode();
 })
 
 function eventListeners(){
@@ -17,4 +18,27 @@ function navegacionResponsive(){
     }else{
         navegacion.classList.add('mostrar');
     }
+}
+
+function darkMode(){
+    const prefiereDarkMode = window.matchMedia('(prefers-color-scheme: dark)');
+    if(prefiereDarkMode){
+        document.body.classList.add('dark-mode');
+    }else{
+        document.body.classList.remove('dark-mode');
+    }
+
+    prefiereDarkMode.addEventListener('change', function(){
+        if(prefiereDarkMode){
+            document.body.classList.add('dark-mode');
+        }else{
+            document.body.classList.remove('dark-mode');
+        }
+    });
+
+    const btn__darkMode = document.querySelector('.dark__mode--boton');
+
+    btn__darkMode.addEventListener('click', function(){
+        document.body.classList.toggle('dark-mode');
+    });
 }
