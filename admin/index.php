6ff4535e2d4ef0302943b93e798899e3a7f97da4
AlonsoER <alonsoer2343@gmail.com>
2022-05-31 15:18:59 -0600

    Listando datos en tabla

--- admin/index.php
+++ admin/index.php
@@ -1,4 +1,14 @@
 <?php 
+    // Importar la DB
+    require '../includes/config/database.php';
+    $db = conectarDB();
+
+    // Query
+    $query = "SELECT * FROM propiedades";
+
+    // Consulta la DB
+    $resultadoConsulta = mysqli_query($db, $query);
+
     $mensaje = $_GET['resultado'] ?? null;
     require '../includes/funciones.php';
     incluirTemplate('header');
@@ -11,6 +21,36 @@
     <?php endif ?>
 
     <a href="/admin/propiedades/crear.php" class="boton boton__verde">Nueva Propiedad</a>
+
+    <table class="propiedades">
+        <thead>
+            <tr>
+                <th>ID</th>
+                <th>Titulo</th>
+                <th>Imagen</th>
+                <th>Precio</th>
+                <th>Acciones</th>
+            </tr>
+        </thead>
+
+        <tbody>
+            <?php while($propiedad = mysqli_fetch_assoc($resultadoConsulta)) : ?>
+            <tr>
+                <td> <?php echo $propiedad['id']; ?> </td>
+                <td> <?php echo $propiedad['titulo']; ?> </td>
+                <td> <img src="../imagenes/<?php echo $propiedad['imagen'] . ".jpg"; ?>" class="imagen-tabla"> </td>
+                <td> <?php echo $propiedad['precio']; ?> </td>
+                <td>
+                    <a href="#" class="boton__rojo--block">Eliminar</a>
+                    <a href="#" class="boton__amarillo--block">Actualizar</a>
+                </td>
+            </tr>
+            <?php endwhile ?>
+        </tbody>
+    </table>
 </main>
 
-<?php incluirTemplate('footer');?>
+<?php 
+    mysqli_close($db);
+    incluirTemplate('footer');
+?>
